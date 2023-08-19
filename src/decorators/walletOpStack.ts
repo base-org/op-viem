5b7188a4e6eabe3a4c31874950e98e7d6a4afa91
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-08-19 15:45:14 -0700

    :sparkles: feat: Add walletOpStack

---
+++ src/decorators/walletOpStack.ts
@@ -0,0 +1,25 @@
+import { Account, Chain, Transport } from 'viem'
+import { WalletClient } from 'wagmi'
+import { bridgeWriteContract } from '../actions/bridgeWriteContract'
+
+/// NOTE We don't currently need account for exisiting actions but keeping in case
+// TODO need to add generics
+export type WalletOpStackActions = {
+  bridgeWriteContract: (
+    // TODO name these params
+    args: Parameters<typeof bridgeWriteContract>[1],
+  ) => Promise<string>
+}
+
+export function publicOpStackActions<
+  TTransport extends Transport = Transport,
+  TChain extends Chain = Chain,
+  TAccount extends Account = Account
+>(
+  client: WalletClient<TTransport, TChain, TAccount>,
+): WalletOpStackActions {
+  return {
+    // TODO do better than as any
+    bridgeWriteContract: (args) => bridgeWriteContract(client as any, args),
+  }
+}
