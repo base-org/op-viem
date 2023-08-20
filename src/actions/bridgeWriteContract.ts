d1b2070ba3360a9dc01a95ead405f2ea0e3e635d
Will Cory <willcory10@gmail.com>
2023-08-19 17:32:07 -0700

    :sparkles: feat: Add bridge way of calling contracts (#3)
    * :sparkles: feat: Add bridge way of calling contracts
    
    * :bug: fix: copy pasta
    
    * :sparkles: feat: Add walletOpStack
    
    ---------
    
    Co-authored-by: Will Cory <willcory@Wills-MacBook-Pro.local>

---
+++ src/actions/bridgeWriteContract.ts
@@ -0,0 +1,50 @@
+import {
+  Chain,
+  Transport,
+  encodeFunctionData,
+  WriteContractParameters,
+  Account,
+  Abi,
+  EncodeFunctionDataParameters,
+  WalletClient,
+} from 'viem'
+import { writeContract } from 'viem/actions'
+import { l1CrossDomainMessengerABI } from '@eth-optimism/contracts-ts'
+import { OpChainL2, OpChainL1 } from '@roninjin10/rollup-chains'
+
+export async function bridgeWriteContract<
+  TAbi extends Abi | readonly unknown[] = Abi,
+  TFunctionName extends string = string,
+  TChainL1 extends OpChainL1 | undefined = OpChainL1,
+  TChainL2 extends OpChainL2 | undefined = OpChainL2,
+  TAccount extends Account | undefined = Account | undefined,
+  TChainOverride extends Chain | undefined = Chain | undefined,
+>(
+  client: WalletClient<Transport, TChainL1>,
+  // TODO make this take an l2Chain that is decorated with l2 info such as the l1 contract addreses
+  { toChain, args, abi, address, functionName, ...restArgs }: { toChain: TChainL2 } & WriteContractParameters<TAbi, TFunctionName, TChainL1, TAccount, TChainOverride>,
+): Promise<string> {
+  const minGasLimit = 200_000
+  const message = encodeFunctionData({
+    abi,
+    functionName,
+    args,
+  } as unknown as EncodeFunctionDataParameters<TAbi, TFunctionName>)
+  const l1TxHash = writeContract(client, {
+    abi: l1CrossDomainMessengerABI,
+    // TODO currently hardcoded for OP should get this from the l2 chain object
+    address: toChain?.opContracts.L1CrossDomainMessengerProxy,
+    functionName: 'sendMessage' as any,
+    args: [
+      address,
+      message,
+      minGasLimit,
+    ],
+    ...restArgs
+    // TODO better types
+  } as any)
+  // compose with getL2Hash method to get l2 hash I think is what we want here
+  // We could consider baking that into this method
+  return l1TxHash
+}
+
