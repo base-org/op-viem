7e47f19d0e5a50f523c61763127e4f1e11b785f1
Wilson Cusack <cusackw@gmail.com>
2023-08-22 19:28:12 -0400

    copy viem and upgrade testing setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,25 @@
-export default {
-  testDir: 'test', // The directory where your test files are located
-  testMatch: '**/*.test.ts', // The pattern to find test files
-}
+export const poolId = Number(process.env.VITEST_POOL_ID ?? 1)
+export const localHttpUrl = `http://127.0.0.1:8545/${poolId}`
+export const localWsUrl = `ws://127.0.0.1:8545/${poolId}`
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    benchmark: {
+      outputFile: './bench/report.json',
+      reporters: process.env.CI ? ['json'] : ['verbose'],
+    },
+    coverage: {
+      reporter: process.env.CI ? ['lcov'] : ['text', 'json', 'html'],
+      exclude: [
+        '**/errors/utils.ts',
+        '**/dist/**',
+        '**/*.test.ts',
+        '**/_test/**',
+      ],
+    },
+    environment: 'node',
+    globalSetup: ['./src/_test/globalSetup.ts'],
+    testTimeout: 10_000,
+  },
+})
