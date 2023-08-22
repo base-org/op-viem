1309de1d99187bffd234060b191bd525cdc12785
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-22 13:25:15 -0400

    cast to any

--- src/actions/wallet/unsafeDepositTransaction.ts
+++ src/actions/wallet/unsafeDepositTransaction.ts
@@ -67,5 +67,5 @@ export async function writeUnsafeDepositTransaction<
     functionName: 'depositTransaction',
     args: [to, value, gasLimit, isCreation, data],
     ...rest,
-  } as unknown as WriteContractParameters<typeof optimismPortalABI, 'depositTransaction', TChain, Account | undefined, TChainOverride>)
+  } as any)
 }
