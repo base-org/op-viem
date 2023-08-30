c0c1bc63de9097574000bd5382a751d58b4ee59d
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-29 23:29:45 -0400

    started on site

--- site/.vitepress/cache/deps/vitepress___@vue_devtools-api.js
+++ site/.vitepress/cache/deps/vitepress___@vue_devtools-api.js
@@ -37,6 +37,7 @@ function now() {
 // ../node_modules/.pnpm/@vue+devtools-api@6.5.0/node_modules/@vue/devtools-api/lib/esm/proxy.js
 var ApiProxy = class {
   constructor(plugin, hook) {
+<<<<<<< HEAD
     this.target = null;
     this.targetQueue = [];
     this.onQueue = [];
@@ -159,4 +160,127 @@ export {
   now,
   setupDevtoolsPlugin
 };
+=======
+    this.target = null
+    this.targetQueue = []
+    this.onQueue = []
+    this.plugin = plugin
+    this.hook = hook
+    const defaultSettings = {}
+    if (plugin.settings) {
+      for (const id in plugin.settings) {
+        const item = plugin.settings[id]
+        defaultSettings[id] = item.defaultValue
+      }
+    }
+    const localSettingsSaveId = `__vue-devtools-plugin-settings__${plugin.id}`
+    let currentSettings = Object.assign({}, defaultSettings)
+    try {
+      const raw = localStorage.getItem(localSettingsSaveId)
+      const data = JSON.parse(raw)
+      Object.assign(currentSettings, data)
+    } catch (e) {}
+    this.fallbacks = {
+      getSettings() {
+        return currentSettings
+      },
+      setSettings(value) {
+        try {
+          localStorage.setItem(localSettingsSaveId, JSON.stringify(value))
+        } catch (e) {}
+        currentSettings = value
+      },
+      now() {
+        return now()
+      },
+    }
+    if (hook) {
+      hook.on(HOOK_PLUGIN_SETTINGS_SET, (pluginId, value) => {
+        if (pluginId === this.plugin.id) {
+          this.fallbacks.setSettings(value)
+        }
+      })
+    }
+    this.proxiedOn = new Proxy(
+      {},
+      {
+        get: (_target, prop) => {
+          if (this.target) {
+            return this.target.on[prop]
+          } else {
+            return (...args) => {
+              this.onQueue.push({
+                method: prop,
+                args,
+              })
+            }
+          }
+        },
+      },
+    )
+    this.proxiedTarget = new Proxy(
+      {},
+      {
+        get: (_target, prop) => {
+          if (this.target) {
+            return this.target[prop]
+          } else if (prop === 'on') {
+            return this.proxiedOn
+          } else if (Object.keys(this.fallbacks).includes(prop)) {
+            return (...args) => {
+              this.targetQueue.push({
+                method: prop,
+                args,
+                resolve: () => {},
+              })
+              return this.fallbacks[prop](...args)
+            }
+          } else {
+            return (...args) => {
+              return new Promise((resolve) => {
+                this.targetQueue.push({
+                  method: prop,
+                  args,
+                  resolve,
+                })
+              })
+            }
+          }
+        },
+      },
+    )
+  }
+  async setRealTarget(target) {
+    this.target = target
+    for (const item of this.onQueue) {
+      this.target.on[item.method](...item.args)
+    }
+    for (const item of this.targetQueue) {
+      item.resolve(await this.target[item.method](...item.args))
+    }
+  }
+}
+
+// ../node_modules/.pnpm/@vue+devtools-api@6.5.0/node_modules/@vue/devtools-api/lib/esm/index.js
+function setupDevtoolsPlugin(pluginDescriptor, setupFn) {
+  const descriptor = pluginDescriptor
+  const target = getTarget()
+  const hook = getDevtoolsGlobalHook()
+  const enableProxy = isProxyAvailable && descriptor.enableEarlyProxy
+  if (hook && (target.__VUE_DEVTOOLS_PLUGIN_API_AVAILABLE__ || !enableProxy)) {
+    hook.emit(HOOK_SETUP, pluginDescriptor, setupFn)
+  } else {
+    const proxy = enableProxy ? new ApiProxy(descriptor, hook) : null
+    const list = (target.__VUE_DEVTOOLS_PLUGINS__ =
+      target.__VUE_DEVTOOLS_PLUGINS__ || [])
+    list.push({
+      pluginDescriptor: descriptor,
+      setupFn,
+      proxy,
+    })
+    if (proxy) setupFn(proxy.proxiedTarget)
+  }
+}
+export { isPerformanceSupported, now, setupDevtoolsPlugin }
+>>>>>>> 7e2eec2 (started on site)
 //# sourceMappingURL=vitepress___@vue_devtools-api.js.map
