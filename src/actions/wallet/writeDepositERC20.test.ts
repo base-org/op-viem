55c202540b05580051b0d7591f73b273e7529d35
Wilson Cusack <wilson.cusack@coinbase.com>
2023-08-30 15:52:21 -0400

    writeDepositERC20 test

--- src/actions/wallet/writeDepositERC20.test.ts
+++ src/actions/wallet/writeDepositERC20.test.ts
@@ -1,24 +1,43 @@
 import { expect, test } from 'vitest'
-import { walletClient } from '../../_test/utils'
+import { publicClient, testClient, walletClient } from '../../_test/utils'
 import { base } from '@roninjin10/rollup-chains'
-import { accounts } from '../../_test/constants'
 import { writeDepositERC20 } from './writeDepositERC20'
+import { mine, writeContract } from 'viem/actions'
+import { erc20ABI } from 'wagmi'
 
-const CBETHL1 = '0xbe9895146f7af43049ca1c1ae358b0541ea49704'
-const CBETHl2 = '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22'
+const USDCL1 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const USDCL2 = '0x2e668bb88287675e34c8df82686dfd0b7f0c0383'
+const zenaddress = '0xFd4F24676eD4588928213F37B126B53c07186F45'
 
 test('default', async () => {
-  expect(
-    await writeDepositERC20(walletClient, {
-      args: {
-        l1Token: CBETHL1,
-        l2Token: CBETHl2,
-        amount: 1n,
-        gasLimit: 1n,
-        data: '0x',
-      },
-      toChain: base,
-      account: accounts[0].address,
-    }),
-  ).toBeDefined()
+  await testClient.impersonateAccount({
+    address: zenaddress,
+  })
+  await testClient.setBalance({
+    address: zenaddress,
+    value: 1000000000000000000n,
+  })
+  await writeContract(testClient, {
+    address: USDCL1,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: [base.opContracts.L1StandardBridgeProxy, 10000n],
+    account: zenaddress,
+  })
+  await mine(testClient, { blocks: 1 })
+  const hash = await writeDepositERC20(walletClient, {
+    args: {
+      l1Token: USDCL1,
+      l2Token: USDCL2,
+      amount: 1n,
+      gasLimit: 1n,
+      data: '0x',
+    },
+    toChain: base,
+    account: zenaddress,
+  })
+  await mine(testClient, { blocks: 1 })
+  expect((await publicClient.getTransactionReceipt({ hash })).status).toEqual(
+    'success',
+  )
 })
