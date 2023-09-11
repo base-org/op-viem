c6a628ba61e6ac971006588fc095e52e23b8ac6a
Wilson Cusack <wilson.cusack@coinbase.com>
2023-09-11 16:09:42 -0400

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export type { DepositTransaction, TransactionDepositedEvent, SourceHashDomain } from './types/depositTransaction.js'
+export type { DepositTransaction, TransactionDepositedEvent } from './types/depositTransaction.js'
+export { SourceHashDomain } from './types/depositTransaction.js'
 export type { GetDepositTransactionParams } from './utils/getDepositTransaction.js'
 export { getDepositTransaction } from './utils/getDepositTransaction.js'
