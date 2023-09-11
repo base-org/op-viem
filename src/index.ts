2b4632ff2fc39c21c801692db0336a086cbce4f1
Wilson Cusack <cusackw@gmail.com>
2023-09-11 14:10:18 -0400

    build and import working, but not seeing types as expected

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { type DepositTransaction, SourceHashDomain, type TransactionDepositedEvent } from './types/depositTransaction.js'
-export { getDepositTransaction } from './utils/getDepositTransaction.js'
+export { getDepositTransaction, type getDepositTransactionParams } from './utils/getDepositTransaction.js'
