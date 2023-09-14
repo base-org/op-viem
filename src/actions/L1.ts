5fd06e8bb6655e97210b50789ff9c656acdbfce4
Wilson Cusack <wilson.cusack@coinbase.com>
2023-09-14 09:36:59 -0400

    export L1 actions

---
+++ src/actions/L1.ts
@@ -0,0 +1,23 @@
+export {
+  getL2HashesForDepositTx,
+  type GetL2HashesForDepositTxParamters,
+  type GetL2HashesForDepositTxReturnType,
+} from './public/L1/getL2HashesForDepositTx.js'
+
+export {
+  getOutputForL2Block,
+  type GetOutputForL2BlockParameters,
+  type GetOutputForL2BlockReturnType,
+  type Proposal,
+} from './public/L1/getOutputForL2Block.js'
+export {
+  simulateDepositERC20,
+  type SimulateDepositERC20Parameters,
+  type SimulateDepositERC20ReturnType,
+} from './public/L1/simulateDepositERC20.js'
+export {
+  type simulateDepositETH,
+  type SimulateDepositETHParameters,
+  type SimulateDepositETHReturnType,
+} from './public/L1/simulateDepositETH.js'
+export { simulateOpStackL1, type SimulateOpStackL1Parameters } from './public/L1/simulateOpStackL1.js'
