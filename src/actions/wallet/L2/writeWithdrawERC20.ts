2061f7be04ed74a23400d4aa2530bbf6e5b5db87
wilsoncusack <cusackw@gmail.com>
2023-09-14 13:39:53 -0400

    writeOpStackL2 (#73)

--- src/actions/wallet/L2/writeWithdrawERC20.ts
+++ src/actions/wallet/L2/writeWithdrawERC20.ts
@@ -1,8 +1,8 @@
-import type { Account, Chain, Transport, WalletClient, WriteContractParameters, WriteContractReturnType } from 'viem'
-import { writeContract } from 'viem/actions'
+import type { Account, Chain, Transport, WalletClient, WriteContractReturnType } from 'viem'
 import type { L2WriteContractParameters } from '../../../types/l2Actions.js'
-import { opStackL2ChainContracts } from '../../../types/opStackContracts.js'
+import { OpStackL2Contract } from '../../../types/opStackContracts.js'
 import { ABI, FUNCTION, type WithdrawToParameters } from '../../../types/withdrawTo.js'
+import { writeOpStackL2, type WriteOpStackL2Parameters } from './writeOpStackL2.js'
 
 export type WriteWithdrawERC20Parameters<
   TChain extends Chain | undefined = Chain,
@@ -24,15 +24,13 @@ export async function writeWithdrawERC20<
   TAccount,
   TChainOverride
 >): Promise<WriteContractReturnType> {
-  return writeContract(client, {
+  return writeOpStackL2(client, {
     abi: ABI,
     functionName: FUNCTION,
     args: [l2Token, to, amount, minGasLimit, extraData],
-    // TODO(Wilson): consider pulling this from chain.contracts
-    // so that it will have a better error if they are not on an L2 chain
-    address: opStackL2ChainContracts.l2StandardBridge.address,
+    contract: OpStackL2Contract.L2StandardBridge,
     ...rest,
-  } as unknown as WriteContractParameters<
+  } as unknown as WriteOpStackL2Parameters<
     typeof ABI,
     typeof FUNCTION,
     TChain,
