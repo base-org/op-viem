35beb5318b908e15bcd32b4fa3f84798cb3e826a
Sabnock <24715302+Sabnock01@users.noreply.github.com>
2023-09-16 16:01:41 -0500

    feat: readFinalizedWithdrawals (#94)
    * feat: readFinalizedWithdrawals
    
    * make suggested changes
    
    * Update src/actions/public/L1/readFinalizedWithdrawals.test.ts
    
    Co-authored-by: wilsoncusack <cusackw@gmail.com>
    
    ---------
    
    Co-authored-by: wilsoncusack <cusackw@gmail.com>

---
+++ src/actions/public/L1/readFinalizedWithdrawals.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import { publicClient } from '../../../_test/utils.js'
+import { base } from '../../../chains/base.js'
+import { readFinalizedWithdrawals } from './readFinalizedWithdrawals.js'
+
+test('read finalized withdrawals', async () => {
+  const finalizedWithdrawal = await readFinalizedWithdrawals(publicClient, {
+    l2Chain: base,
+    withdrawalHash: '0xEC0AD491512F4EDC603C2DD7B9371A0B18D4889A23E74692101BA4C6DC9B5709',
+  })
+
+  expect(finalizedWithdrawal).toEqual(true)
+})
+
+test('returns false if not finalized', async () => {
+  const finalizedWithdrawal = await readFinalizedWithdrawals(publicClient, {
+    l2Chain: base,
+    withdrawalHash: '0xEC0AD491512F4EDC603C2DD7B9371A0C18D4889A23E74692101BA4C6DC9B5709',
+  })
+
+  expect(finalizedWithdrawal).toEqual(false)
+})
--- src/actions/public/L1/readFinalizedWithdrawals.ts
+++ src/actions/public/L1/readFinalizedWithdrawals.ts
@@ -1,5 +1,5 @@
 import { optimismPortalABI } from '@eth-optimism/contracts-ts'
-import type { Chain, Hex, PublicClient, Transport } from 'viem'
+import type { Chain, PublicClient, Transport } from 'viem'
 import type { MessagePassedEvent } from '../../../index.js'
 import type { GetL2Chain, L1ActionBaseType } from '../../../types/l1Actions.js'
 import { OpStackL1Contract } from '../../../types/opStackContracts.js'
@@ -7,9 +7,9 @@ import { readOpStackL1, type ReadOpStackL1Parameters } from './readOpStackL1.js'
 
 const ABI = optimismPortalABI
 const CONTRACT = OpStackL1Contract.OptimismPortal
-const FUNCTION_NAME = 'provenWithdrawals'
+const FUNCTION_NAME = 'finalizedWithdrawals'
 
-export type ReadProvenWithdrawalsParameters<
+export type ReadFinalizedWithdrawalsParameters<
   TChain extends Chain | undefined = Chain,
 > =
   & { withdrawalHash: MessagePassedEvent['withdrawalHash'] }
@@ -18,24 +18,15 @@ export type ReadProvenWithdrawalsParameters<
     typeof CONTRACT
   >
 
-export type ProvenWithdrawal = {
-  outputRoot: Hex
-  timestamp: bigint
-  l2OutputIndex: bigint
-}
-
-export type ReadProvenWithdrawalsReturnType = ProvenWithdrawal
-
-// Convention: use `read` if this is just 1:1 with some contract function
-export async function readProvenWithdrawals<TChain extends Chain | undefined>(
+export async function readFinalizedWithdrawals<TChain extends Chain | undefined>(
   client: PublicClient<Transport, TChain>,
   {
     withdrawalHash,
     optimismPortalAddress,
     l2Chain,
-  }: ReadProvenWithdrawalsParameters<TChain>,
-): Promise<ReadProvenWithdrawalsReturnType> {
-  const values = await readOpStackL1(client, {
+  }: ReadFinalizedWithdrawalsParameters<TChain>,
+): Promise<boolean> {
+  const finalizedWithdrawal = await readOpStackL1(client, {
     contract: CONTRACT,
     abi: ABI,
     functionName: FUNCTION_NAME,
@@ -45,15 +36,5 @@ export async function readProvenWithdrawals<TChain extends Chain | undefined>(
     chain: client.chain,
   } as ReadOpStackL1Parameters<TChain, typeof ABI, typeof FUNCTION_NAME>)
 
-  const provenWithdrawal = {
-    outputRoot: values[0],
-    timestamp: values[1],
-    l2OutputIndex: values[2],
-  }
-
-  if (provenWithdrawal.timestamp === 0n) {
-    throw new Error(`Withdrawal with hash ${withdrawalHash} is not proven`)
-  }
-
-  return provenWithdrawal
+  return finalizedWithdrawal
 }
