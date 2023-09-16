aa71abab468a5abacf5f99eb7ade5ac51ebe2c8c
Wilson Cusack <cusackw@gmail.com>
2023-09-16 16:38:04 -0400

    also allow getWithdrawalMessages to take txReceipt

--- src/actions/public/L2/getWithdrawalMessages.test.ts
+++ src/actions/public/L2/getWithdrawalMessages.test.ts
@@ -2,9 +2,27 @@ import { expect, test } from 'vitest'
 import { rollupPublicClient } from '../../../_test/utils.js'
 import { getWithdrawalMessages } from './getWithdrawalMessages.js'
 
-test('correctly retrieves L2 hash', async () => {
+test('correctly retrieves messages from hash', async () => {
   const messages = await getWithdrawalMessages(rollupPublicClient, {
     hash: '0x999bab960dbdf600c51371ae819957063337a50cec2eb8032412739defadabe7',
+  })
+  expect(messages.blockNumber).toEqual(2725977n)
+  expect(messages.messages.length).toEqual(1)
+  expect(messages.messages[0].nonce).toBeDefined()
+  expect(messages.messages[0].gasLimit).toBeDefined()
+  expect(messages.messages[0].data).toBeDefined()
+  expect(messages.messages[0].value).toBeDefined()
+  expect(messages.messages[0].sender).toBeDefined()
+  expect(messages.messages[0].target).toBeDefined()
+  expect(messages.messages[0].withdrawalHash).toBeDefined()
+})
+
+test('correctly retrieves messages from receipt', async () => {
+  const txReceipt = await rollupPublicClient.getTransactionReceipt({
+    hash: '0x999bab960dbdf600c51371ae819957063337a50cec2eb8032412739defadabe7',
+  })
+  const messages = await getWithdrawalMessages(rollupPublicClient, {
+    txReceipt,
   })
   expect(messages.blockNumber).toEqual(2725977n)
   expect(messages.messages.length).toEqual(1)
