d5b5b01680d82d406adbcde8ea3381a38e40edfd
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-09-18 03:30:56 -0500

    :white_check_mark: chore: add resolveL1OpStackContractAddress test

---
+++ src/utils/resolveL1OpStackContractAddress.test.ts
@@ -0,0 +1,43 @@
+import { expect, test } from 'vitest'
+import { publicClient } from '../_test/utils.js'
+import { base } from '../chains/base.js'
+import { OpStackL1Contract } from '../types/opStackContracts.js'
+import { resolveL1OpStackContractAddress } from './resolveL1OpStackContractAddress.js'
+
+test('resolves Base OptimismPortal contract address', async () => {
+  const resolvedPortalAddress = await resolveL1OpStackContractAddress(
+    {
+      l2Chain: base,
+      chain: publicClient.chain,
+      contract: OpStackL1Contract.OptimismPortal,
+    },
+  )
+
+  expect(resolvedPortalAddress).toEqual('0x49048044D57e1C92A77f79988d21Fa8fAF74E97e')
+})
+
+test('raises error if chain undefined', async () => {
+  await expect(async () =>
+    resolveL1OpStackContractAddress(
+      {
+        l2Chain: undefined,
+        chain: publicClient.chain,
+        contract: OpStackL1Contract.OptimismPortal,
+      },
+    )
+  ).rejects.toThrowError('Must provide either l2Chain or optimismPortalAddress')
+})
+
+test('raises error if L1 mismatch', async () => {
+  publicClient.chain.id = 0
+
+  await expect(async () =>
+    resolveL1OpStackContractAddress(
+      {
+        l2Chain: base,
+        chain: publicClient.chain,
+        contract: OpStackL1Contract.OptimismPortal,
+      },
+    )
+  ).rejects.toThrowError('Chain ID "0" does not match expected L1 chain ID "1"')
+})
