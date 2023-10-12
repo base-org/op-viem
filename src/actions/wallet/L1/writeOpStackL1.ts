a2bacb9adb8fe0ce571cd3697052927bb85597ff
Matthew Bunday <matthew.bunday@coinbase.com>
2023-10-12 15:43:00 -0400

    Rip out resolveL1OpStackContractAddress

--- src/actions/wallet/L1/writeOpStackL1.ts
+++ src/actions/wallet/L1/writeOpStackL1.ts
@@ -9,12 +9,7 @@ import type {
   WriteContractReturnType,
 } from 'viem'
 import { writeContract } from 'viem/actions'
-import type { GetL2Chain, ResolveChain } from '../../../types/l1Actions.js'
 import { OpStackL1Contract } from '../../../types/opStackContracts.js'
-import {
-  resolveL1OpStackContractAddress,
-  type ResolveL1OpStackContractAddressParameters,
-} from '../../../utils/resolveL1OpStackContractAddress.js'
 
 export type WriteOpStackL1Parameters<
   TChain extends Chain | undefined = Chain,
@@ -23,14 +18,7 @@ export type WriteOpStackL1Parameters<
   TAbi extends Abi | readonly unknown[] = Abi,
   TFunctionName extends string = string,
 > =
-  & { contract: OpStackL1Contract; chain: TChain | TChainOverride }
-  & ({
-    l2Chain: GetL2Chain<ResolveChain<TChain, TChainOverride>>
-    address?: never
-  } | {
-    l2Chain?: never
-    address: Address
-  })
+  & { contract: OpStackL1Contract; chain: TChain | TChainOverride; address: Address }
   & Omit<
     WriteContractParameters<
       TAbi,
@@ -51,18 +39,12 @@ export function writeOpStackL1<
 >(
   client: WalletClient<Transport, TChain, TAccount>,
   {
-    l2Chain,
-    contract,
     address,
-    chain = client.chain,
     ...rest
   }: WriteOpStackL1Parameters<TChain, TAccount, TChainOverride, TAbi, TFunctionName>,
 ): Promise<WriteContractReturnType> {
-  const resolvedAddress = resolveL1OpStackContractAddress(
-    { l2Chain, chain, contract, address } as ResolveL1OpStackContractAddressParameters<TChain, TChainOverride>,
-  )
   return writeContract(client, {
-    address: resolvedAddress,
+    address,
     ...rest,
   } as unknown as WriteContractParameters<
     TAbi,
