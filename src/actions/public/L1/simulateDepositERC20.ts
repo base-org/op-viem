a27f41ef3a9ee9cd799cb2534046c05c1bdb1c8d
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2023-10-25 14:20:17 -0400

    deposit erc20 v2

--- src/actions/public/L1/simulateDepositERC20.ts
+++ src/actions/public/L1/simulateDepositERC20.ts
@@ -1,4 +1,13 @@
-import type { Chain, PublicClient, SimulateContractParameters, SimulateContractReturnType, Transport } from 'viem'
+import type {
+  Account,
+  Address,
+  Chain,
+  ContractFunctionArgs,
+  PublicClient,
+  SimulateContractParameters,
+  SimulateContractReturnType,
+  Transport,
+} from 'viem'
 import { simulateContract } from 'viem/actions'
 import { type RawOrContractAddress, resolveAddress } from '../../../types/addresses.js'
 import { ABI, type DepositERC20Parameters, FUNCTION } from '../../../types/depositERC20.js'
@@ -7,15 +16,36 @@ import type { L1SimulateActionBaseType } from '../../../types/l1Actions.js'
 export type SimulateDepositERC20Parameters<
   TChain extends Chain | undefined = Chain,
   TChainOverride extends Chain | undefined = Chain | undefined,
+  TAccountOverride extends Account | Address | undefined = undefined,
   _chainId = TChain extends Chain ? TChain['id'] : number,
 > =
   & { args: DepositERC20Parameters; l1StandardBridge: RawOrContractAddress<_chainId> }
-  & L1SimulateActionBaseType<TChain, TChainOverride, typeof ABI, typeof FUNCTION>
+  & L1SimulateActionBaseType<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'nonpayable', 'depositERC20To'>,
+    TChain,
+    TChainOverride,
+    TAccountOverride
+  >
 
 export type SimulateDepositERC20ReturnType<
-  TChain extends Chain | undefined,
-  TChainOverride extends Chain | undefined = undefined,
-> = SimulateContractReturnType<typeof ABI, typeof FUNCTION, TChain, TChainOverride>
+  TChain extends Chain | undefined = Chain | undefined,
+  TAccount extends Account | undefined = Account | undefined,
+  TChainOverride extends Chain | undefined = Chain | undefined,
+  TAccountOverride extends Account | Address | undefined =
+    | Account
+    | Address
+    | undefined,
+> = SimulateContractReturnType<
+  typeof ABI,
+  typeof FUNCTION,
+  ContractFunctionArgs<typeof ABI, 'nonpayable', 'depositERC20To'>,
+  TChain,
+  TAccount,
+  TChainOverride,
+  TAccountOverride
+>
 
 /**
  * Simulates a deposit of ERC20 tokens to L2
@@ -24,20 +54,47 @@ export type SimulateDepositERC20ReturnType<
  */
 export async function simulateDepositERC20<
   TChain extends Chain | undefined,
-  TChainOverride extends Chain | undefined = undefined,
+  TAccount extends Account | undefined,
+  TChainOverride extends Chain | undefined,
+  TAccountOverride extends Account | Address | undefined = undefined,
 >(
   client: PublicClient<Transport, TChain>,
   {
     args: { l1Token, l2Token, to, amount, minGasLimit, extraData = '0x' },
     l1StandardBridge,
     ...rest
-  }: SimulateDepositERC20Parameters<TChain, TChainOverride>,
-): Promise<SimulateContractReturnType<typeof ABI, typeof FUNCTION, TChain, TChainOverride>> {
+  }: SimulateDepositERC20Parameters<TChain, TChainOverride, TAccountOverride>,
+): Promise<
+  SimulateContractReturnType<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'nonpayable', 'depositERC20To'>,
+    TChain,
+    TAccount,
+    TChainOverride,
+    TAccountOverride
+  >
+> {
   return simulateContract(client, {
     address: resolveAddress(l1StandardBridge),
     abi: ABI,
     functionName: FUNCTION,
     args: [l1Token, l2Token, to, amount, minGasLimit, extraData],
     ...rest,
-  } as unknown as SimulateContractParameters<typeof ABI, typeof FUNCTION, TChain, TChainOverride>)
+  } as unknown as SimulateContractParameters<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'nonpayable', 'depositERC20To'>,
+    TChain,
+    TChainOverride,
+    TAccountOverride
+  >) as unknown as SimulateContractReturnType<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'nonpayable', 'depositERC20To'>,
+    TChain,
+    TAccount,
+    TChainOverride,
+    TAccountOverride
+  >
 }
