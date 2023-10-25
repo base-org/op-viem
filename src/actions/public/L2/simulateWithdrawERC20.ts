661aef4171b0684a4a4c91108413dd9d7d958cbf
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2023-10-25 16:12:39 -0400

    migrate L2 public actions to v2

--- src/actions/public/L2/simulateWithdrawERC20.ts
+++ src/actions/public/L2/simulateWithdrawERC20.ts
@@ -1,4 +1,13 @@
-import type { Chain, PublicClient, SimulateContractParameters, SimulateContractReturnType, Transport } from 'viem'
+import type {
+  Account,
+  Address,
+  Chain,
+  ContractFunctionArgs,
+  PublicClient,
+  SimulateContractParameters,
+  SimulateContractReturnType,
+  Transport,
+} from 'viem'
 import { simulateContract } from 'viem/actions'
 import type { L2SimulateContractParameters } from '../../../types/l2Actions.js'
 import { opStackL2ChainContracts } from '../../../types/opStackContracts.js'
@@ -7,14 +16,35 @@ import { ABI, FUNCTION, type WithdrawToParameters } from '../../../types/withdra
 export type SimulateWithdrawERC20Parameters<
   TChain extends Chain | undefined = Chain,
   TChainOverride extends Chain | undefined = Chain | undefined,
+  TAccountOverride extends Account | Address | undefined = undefined,
 > =
   & { args: WithdrawToParameters }
-  & L2SimulateContractParameters<typeof ABI, typeof FUNCTION, TChain, TChainOverride>
+  & L2SimulateContractParameters<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'payable', typeof FUNCTION>,
+    TChain,
+    TChainOverride,
+    TAccountOverride
+  >
 
 export type SimulateWithdrawERC20ReturnType<
-  TChain extends Chain | undefined,
-  TChainOverride extends Chain | undefined = undefined,
-> = SimulateContractReturnType<typeof ABI, typeof FUNCTION, TChain, TChainOverride>
+  TChain extends Chain | undefined = Chain | undefined,
+  TAccount extends Account | undefined = Account | undefined,
+  TChainOverride extends Chain | undefined = Chain | undefined,
+  TAccountOverride extends Account | Address | undefined =
+    | Account
+    | Address
+    | undefined,
+> = SimulateContractReturnType<
+  typeof ABI,
+  typeof FUNCTION,
+  ContractFunctionArgs<typeof ABI, 'payable', typeof FUNCTION>,
+  TChain,
+  TAccount,
+  TChainOverride,
+  TAccountOverride
+>
 
 /**
  * Simulates a withdrawal of ERC20 tokens to an L1 address.
@@ -26,15 +56,18 @@ export type SimulateWithdrawERC20ReturnType<
  * @param {Hex} [extraData] the extra data for the withdrawal
  */
 export async function simulateWithdrawERC20<
-  TChain extends Chain | undefined = Chain,
-  TChainOverride extends Chain | undefined = Chain | undefined,
+  TChain extends Chain | undefined,
+  TAccount extends Account | undefined,
+  TChainOverride extends Chain | undefined,
+  TAccountOverride extends Account | Address | undefined = undefined,
 >(client: PublicClient<Transport, TChain>, {
   args: { l2Token, to, amount, minGasLimit, extraData = '0x' },
   ...rest
 }: SimulateWithdrawERC20Parameters<
   TChain,
-  TChainOverride
->): Promise<SimulateWithdrawERC20ReturnType<TChain, TChainOverride>> {
+  TChainOverride,
+  TAccountOverride
+>): Promise<SimulateWithdrawERC20ReturnType<TChain, TAccount, TChainOverride, TAccountOverride>> {
   return simulateContract(client, {
     abi: ABI,
     functionName: FUNCTION,
@@ -44,7 +77,16 @@ export async function simulateWithdrawERC20<
   } as unknown as SimulateContractParameters<
     typeof ABI,
     typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'payable', typeof FUNCTION>,
     TChain,
     TChainOverride
-  >)
+  >) as unknown as SimulateContractReturnType<
+    typeof ABI,
+    typeof FUNCTION,
+    ContractFunctionArgs<typeof ABI, 'payable', typeof FUNCTION>,
+    TChain,
+    TAccount,
+    TChainOverride,
+    TAccountOverride
+  >
 }
--- src/types/l2Actions.ts
+++ src/types/l2Actions.ts
@@ -1,6 +1,7 @@
 import type {
   Abi,
   Account,
+  Address,
   Chain,
   ContractFunctionArgs,
   ContractFunctionName,
@@ -10,37 +11,38 @@ import type {
 
 export type L2WriteContractParameters<
   TAbi extends Abi | readonly unknown[] = Abi,
-  TFunctioName extends ContractFunctionName<
+  TFunctionName extends ContractFunctionName<
     TAbi,
     'nonpayable' | 'payable'
   > = ContractFunctionName<TAbi, 'nonpayable' | 'payable'>,
   TArgs extends ContractFunctionArgs<
     TAbi,
     'nonpayable' | 'payable',
-    TFunctioName
-  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctioName>,
+    TFunctionName
+  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctionName>,
   TChain extends Chain | undefined = Chain,
   TAccount extends Account | undefined = Account | undefined,
   TChainOverride extends Chain | undefined = Chain | undefined,
 > = Omit<
-  WriteContractParameters<TAbi, TFunctioName, TArgs, TChain, TAccount, TChainOverride>,
+  WriteContractParameters<TAbi, TFunctionName, TArgs, TChain, TAccount, TChainOverride>,
   'abi' | 'functionName' | 'args' | 'address'
 >
 
 export type L2SimulateContractParameters<
   TAbi extends Abi | readonly unknown[] = Abi,
-  TFunctioName extends ContractFunctionName<
+  TFunctionName extends ContractFunctionName<
     TAbi,
     'nonpayable' | 'payable'
   > = ContractFunctionName<TAbi, 'nonpayable' | 'payable'>,
   TArgs extends ContractFunctionArgs<
     TAbi,
     'nonpayable' | 'payable',
-    TFunctioName
-  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctioName>,
+    TFunctionName
+  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctionName>,
   TChain extends Chain | undefined = Chain,
   TChainOverride extends Chain | undefined = Chain | undefined,
+  TAccountOverride extends Account | Address | undefined = undefined,
 > = Omit<
-  SimulateContractParameters<TAbi, TFunctioName, TArgs, TChain, TChainOverride>,
+  SimulateContractParameters<TAbi, TFunctionName, TArgs, TChain, TChainOverride, TAccountOverride>,
   'abi' | 'functionName' | 'args' | 'address'
 >
