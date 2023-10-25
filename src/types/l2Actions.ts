bcd99a553db4eb7bcff4050f5d0c60e0d0d715c1
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2023-10-25 15:13:37 -0400

    migrate L2 wallet actions to v2

--- src/types/l2Actions.ts
+++ src/types/l2Actions.ts
@@ -1,22 +1,46 @@
-import type { Abi, Account, Chain, SimulateContractParameters, WriteContractParameters } from 'viem'
+import type {
+  Abi,
+  Account,
+  Chain,
+  ContractFunctionArgs,
+  ContractFunctionName,
+  SimulateContractParameters,
+  WriteContractParameters,
+} from 'viem'
 
 export type L2WriteContractParameters<
   TAbi extends Abi | readonly unknown[] = Abi,
-  TFunctioName extends string = string,
+  TFunctioName extends ContractFunctionName<
+    TAbi,
+    'nonpayable' | 'payable'
+  > = ContractFunctionName<TAbi, 'nonpayable' | 'payable'>,
+  TArgs extends ContractFunctionArgs<
+    TAbi,
+    'nonpayable' | 'payable',
+    TFunctioName
+  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctioName>,
   TChain extends Chain | undefined = Chain,
   TAccount extends Account | undefined = Account | undefined,
   TChainOverride extends Chain | undefined = Chain | undefined,
 > = Omit<
-  WriteContractParameters<TAbi, TFunctioName, TChain, TAccount, TChainOverride>,
+  WriteContractParameters<TAbi, TFunctioName, TArgs, TChain, TAccount, TChainOverride>,
   'abi' | 'functionName' | 'args' | 'address'
 >
 
 export type L2SimulateContractParameters<
   TAbi extends Abi | readonly unknown[] = Abi,
-  TFunctioName extends string = string,
+  TFunctioName extends ContractFunctionName<
+    TAbi,
+    'nonpayable' | 'payable'
+  > = ContractFunctionName<TAbi, 'nonpayable' | 'payable'>,
+  TArgs extends ContractFunctionArgs<
+    TAbi,
+    'nonpayable' | 'payable',
+    TFunctioName
+  > = ContractFunctionArgs<TAbi, 'nonpayable' | 'payable', TFunctioName>,
   TChain extends Chain | undefined = Chain,
   TChainOverride extends Chain | undefined = Chain | undefined,
 > = Omit<
-  SimulateContractParameters<TAbi, TFunctioName, TChain, TChainOverride>,
+  SimulateContractParameters<TAbi, TFunctioName, TArgs, TChain, TChainOverride>,
   'abi' | 'functionName' | 'args' | 'address'
 >
